52ce01e48c7f6b63dd942ed7af924f5da4743600
Ovsyannikov Dmitry <dmitroy1234@gmail.com>
2025-04-01 19:03:39 +0300

    Init commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="shortcut icon" href="img/safari-pinned-tab.svg" type="image/x-icon">
+    <title>Регистрация</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    
+</body>
+</html>
