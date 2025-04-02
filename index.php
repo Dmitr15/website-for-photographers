d6840a1499ed06473ad0565e9416cd1486aff914
Ovsyannikov Dmitry <dmitroy1234@gmail.com>
2025-04-02 12:29:44 +0300

    Add about section

--- index.php
+++ index.php
@@ -186,6 +186,15 @@
       <div class="py-4">
       <h2 class="text-center">About</h2>
       </div>
+      <div class="container">
+        <h3 class="text-center">Dan</h3>
+        <p class="text-center">
+          <b>
+            I am a passionate photographer interested in working in Architecture, Nature and Travel.<br>
+            I am fuul time freelancer.
+          </b>
+        </p>
+      </div>
     </div>
   </section>
 
