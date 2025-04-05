37fddd11bfff70eff76684a4a69ffb5465bc14ac
Ovsyannikov Dmitry <dmitroy1234@gmail.com>
2025-04-05 11:10:31 +0300

    Add sessions

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 error_reporting(E_ERROR | E_PARSE);
+session_start();
+unset($_SESSION['form_submit']);
 //require_once("formhandler.php");
 //$errors = formEditor();
 //$description = descriptionHandler();
