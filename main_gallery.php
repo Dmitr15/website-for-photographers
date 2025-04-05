5ef0a107f9d878dc5fe72d4e10b14622892bebdf
Ovsyannikov Dmitry <dmitroy1234@gmail.com>
2025-04-05 11:42:17 +0300

    Add file with carousel and gallery sections

---
+++ main_gallery.php
@@ -0,0 +1,95 @@
+<div id="carouselExampleCaptions" class="carousel slide">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="img/dino-reichmuth-A5rCN8626Ck-unsplash.jpg" class="d-block w-100" alt="Travel img">
+        <div class="carousel-caption d-none d-md-block">
+          <h3>Travel</h3>
+
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="img/lance-anderson-QdAAasrZhdk-unsplash.jpg" class="d-block w-100" alt="Architecture img">
+        <div class="carousel-caption d-none d-md-block">
+          <h3>Architecture</h3>
+
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="img/adam-kool-ndN00KmbJ1c-unsplash.jpg" class="d-block w-100" alt="Nature img">
+        <div class="carousel-caption d-none d-md-block">
+          <h3>Nature</h3>
+
+        </div>
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+
+  <section class="my-4">
+    <div class='py-4'>
+      <h2 class="text-center">Nature</h2>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="img/dawid-zawila--G3rw6Y02D0-unsplash.jpg" class="img-fluid pd-3" alt="nature">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="img/hendrik-cornelissen--qrcOR33ErA-unsplash.jpg" class="img-fluid pd-3" alt="nature">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="img/wil-stewart-pHANr-CpbYM-unsplash.jpg" class="img-fluid pd-3" alt="nature">
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="my-4">
+    <div class='py-4'>
+      <h2 class="text-center">Architecture</h2>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="img/anatolii-nesterov-XtdG5ZJpyoc-unsplash.jpg" class="img-fluid pd-3" alt="nature">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="img/ali-zeynalli-lfnwueL8rfw-unsplash.jpg" class="img-fluid pd-3" alt="nature">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="img/borys-jarzcuk-UqTkotpdE6k-unsplash.jpg" class="img-fluid pd-3" alt="nature">
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="my-4">
+    <div class='py-4'>
+      <h2 class="text-center">Travel</h2>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="img/luca-bravo-O453M2Liufs-unsplash.jpg" class="img-fluid pd-3" alt="travel">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="img/eva-darron-oCdVtGFeDC0-unsplash.jpg" class="img-fluid pd-3" alt="travel">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="img/drif-riadh-YpkuRn54y4w-unsplash.jpg" class="img-fluid pd-3" alt="travel">
+        </div>
+      </div>
+    </div>
+  </section>
