a30aa29a14afbeb48e82167f397070f5547511e6
Ovsyannikov Dmitry <dmitroy1234@gmail.com>
2025-04-05 11:35:14 +0300

    Add about file

---
+++ about.php
@@ -0,0 +1,16 @@
+<section>
+    <div class="my-4">
+      <div class="py-4">
+        <h2 class="text-center">About Me</h2>
+      </div>
+      <div class="container">
+        <h3 class="text-center">Dan</h3>
+        <p class="text-center">
+          <b>
+            I am a passionate photographer interested in working in Architecture, Nature and Travel.<br>
+            I am full time freelancer.
+          </b>
+        </p>
+      </div>
+    </div>
+  </section>
